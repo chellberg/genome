dd365c6ecbecac87d601c7089e31163edde487fa
Corin Chellberg <corinchellberg@gmail.com>
2016-09-13 21:31:41 -0400

    test km1er generation

--- spec.rb
+++ spec.rb
@@ -25,5 +25,25 @@ describe Graph do
 
       assert_equal reassembled_result, string
     end
+
+    it 'returns correct left and right km-1ers' do
+      string = "thequicklazydog"
+      km1Ls = []
+      km1Rs = []
+      kmers = []
+      @graph.chop(string) do |kmer, km1L, km1R|
+        kmers << kmer
+        km1Ls << km1L
+        km1Rs << km1R
+      end
+
+      assert_equal kmers.first, 'thequ'
+      assert_equal km1Ls.first, 'theq'
+      assert_equal km1Rs.first, 'hequ'
+
+      assert_equal kmers.last, 'zydog'
+      assert_equal km1Ls.last, 'zydo'
+      assert_equal km1Rs.last, 'ydog'
+    end
   end
 end
